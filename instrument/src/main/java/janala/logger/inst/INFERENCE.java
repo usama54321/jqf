3a800a8444f3026ed9497743fc4423e2065081fa
usama54321 <usama54321@gmail.com>
2021-10-14 21:28:51 -0700

    add inference result to the object

--- instrument/src/main/java/janala/logger/inst/INFERENCE.java
+++ instrument/src/main/java/janala/logger/inst/INFERENCE.java
@@ -4,12 +4,18 @@ import java.util.Map;
 
 public class INFERENCE extends Instruction {
   int i;
+  Object data;
   public INFERENCE(Object data) {
     super(-1, -1);
+    this.data = data;
   }
 
   public void visit(IVisitor visitor) {
-    visitor.visitINFERENCE(this); 
+    visitor.visitINFERENCE(this);
+  }
+
+  public Object getData() {
+    return this.data;
   }
 
   @Override

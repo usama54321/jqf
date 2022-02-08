be11cf85b3ae47e95d2c1844935a452b04607e4b
usama54321 <usama54321@gmail.com>
2022-02-07 17:48:23 -0800

    fix repro tf guidance and coverage classes for correct logging

--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/repro/ReproTfCoverage.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/repro/ReproTfCoverage.java
@@ -17,23 +17,63 @@ public class ReproTfCoverage extends Coverage {
     public class ClassMethodMap extends HashMap<String, Set<String>> {}
     public class MethodIidMap extends HashMap<String, Set<Integer>> {}
     public class IidBranchMap extends HashMap<Integer, BranchData> {}
+    public Set<BranchData> branchData;
     int currentCoverage;
 
-    public class BranchData {
+    public class BranchData implements Comparable<BranchData> {
         public int trueTaken;
         public int falseTaken;
         public int lineNumber;
+
+        public BranchData() {
+            trueTaken = 0;
+            falseTaken = 0;
+        }
+
+        public int compareTo(BranchData d) {
+            if (this == d)
+                return 0;
+            return lineNumber < d.lineNumber ? -1: 1;
+        }
     };
 
-    public class CoverageData {
+    public class CoverageData implements Comparable<CoverageData> {
         public MethodIidMap methodMapping;
         public IidBranchMap iidMapping;
         public ClassMethodMap classMethodMap;
+        public Map<String, BranchData> branchData;
+        public int lineNumber;
+        public String clazz;
+        public String method;
 
         public CoverageData() {
+            this("", "", -1);
+        }
+
+        @Override
+        public int compareTo(CoverageData o) {
+            if(o.getSignature().equals(getSignature()))
+                return 0;
+
+            return lineNumber < o.lineNumber ? -1 : 1;
+        }
+
+        public CoverageData(String clazz, String method, int lineNumber) {
             methodMapping = new MethodIidMap();
             iidMapping = new IidBranchMap();
             classMethodMap = new ClassMethodMap();
+            branchData = new HashMap<>();
+            this.clazz = clazz;
+            this.method = method;
+            this.lineNumber = lineNumber;
+        }
+
+        public String getSignature() {
+            return this.clazz + "." + this.method + ":" + String.valueOf(this.lineNumber);
+        }
+
+        public void merge(CoverageData d) {
+            //
         }
     }
 
@@ -60,14 +100,46 @@ public class ReproTfCoverage extends Coverage {
         coverageData = new ArrayList<>();
         coverageData.add(new CoverageData());
         currentCoverage = 0;
+        branchData = new TreeSet<>();
     }
 
+    public void addToCoverage(String cls, String methodName, int iid, int arm, int lineNumber, CoverageData cv) {
+        String signature = String.format("%s.%s:%s", cls, methodName, iid);
+
+        CoverageData data = getCurrentCoverage();
+
+        Map<String, BranchData> branchData = data.branchData;
+        BranchData kv = null;
+
+        if(branchData.containsKey(signature)) {
+            kv = branchData.get(signature);
+        } else {
+            kv = new BranchData();
+        }
+
+        kv.trueTaken += (arm == 0 ? 1 : 0);
+        kv.falseTaken += (arm == 0 ? 0: 1);
+        branchData.put(signature, kv);
+        kv.lineNumber = lineNumber;
+
+        this.branchData.add(kv);
+    }
+
+    private CoverageData getCurrentCoverage() {
+        return coverageData.get(currentCoverage);
+    }
     //@TODO record classes too
     @Override
     public void visitBranchEvent(BranchEvent e) {
+        if (e.getContainingClass().contains("tensorflowlite") || e.getContainingClass().contains("GraphRunner"))
+            return;
         ClassMethodMap classMethodMap = getClassMethodMap();
         MethodIidMap methodMapping = getMethodMapping();
         IidBranchMap iidMapping = getIidMapping();
+
+
+        addToCoverage(e.getContainingClass(), e.getContainingMethodName(), e.getLineNumber(), e.getArm(), e.getLineNumber(), getCurrentCoverage());
+        /*
 
         String cls = e.getContainingClass();
         String methodName = e.getContainingMethodName() + "." + e.getContainingMethodDesc();
@@ -112,23 +184,53 @@ public class ReproTfCoverage extends Coverage {
 
         //mapping.put(key, count);
         //super.visitBranchEvent(e);
+        */
 
+        /*
+        if(testCount % 1000 == 0) {
+            System.out.printf("methods size %d\n", methodMapping.size());
+            for(Map.Entry<String, Set<Integer>> kv: methodMapping.entrySet()) {
+                System.out.printf("Method %s\n", kv.getKey());
+                Set<Integer> iids = kv.getValue();
+                System.out.printf("iid left right\n");
+                for(Integer iid: iids) {
+                    AbstractMap.SimpleEntry<Integer, Integer> nkv = iidMapping.get(iid);
+                    System.out.printf("%d %d %d\n", iid, nkv.getKey(), nkv.getValue());
+                }
+                System.out.printf("\n");
+            }
+                //System.out.printf("%s %d\n", kv.getKey(), kv.getValue());
+        }*/
     }
 
     public void setCurrentCoverage(int index) {
         currentCoverage = index;
     }
 
-    private void addNewCoverage() {
+    private void addNewCoverage(InferenceEvent e) {
         //System.out.println("adding new coverage");
-        coverageData.add(new CoverageData());
+        coverageData.add(new CoverageData(e.clazz, e.method, e.getLineNumber()));
     }
 
     @Override
     public void visitInferenceEvent(InferenceEvent e) {
-        int newIndex = currentCoverage + 1;
+        int index = 0;
+        int newIndex = -1;
+
+        for(CoverageData d: coverageData) {
+            if (d.getSignature().equals(e.clazz + "." + e.method + ":" + String.valueOf(e.getLineNumber()))) {
+                newIndex = index;
+                break;
+            }
+            index++;
+        }
+
+        //if can not find coverage for this signature
+        if (newIndex == -1)
+            newIndex = currentCoverage + 1;
+
         if (newIndex >= coverageData.size()) {
-            addNewCoverage();
+            addNewCoverage(e);
         }
         setCurrentCoverage(newIndex);
     }
--- fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/repro/ReproTfGuidance.java
+++ fuzz/src/main/java/edu/berkeley/cs/jqf/fuzz/repro/ReproTfGuidance.java
@@ -12,6 +12,7 @@ import java.util.Set;
 
 import edu.berkeley.cs.jqf.fuzz.repro.ReproTfCoverage.IidBranchMap;
 import edu.berkeley.cs.jqf.fuzz.repro.ReproTfCoverage.MethodIidMap;
+import edu.berkeley.cs.jqf.fuzz.guidance.Result;
 import edu.berkeley.cs.jqf.fuzz.repro.ReproTfCoverage.ClassMethodMap;
 import edu.berkeley.cs.jqf.fuzz.util.IOUtils;
 
@@ -19,12 +20,14 @@ import edu.berkeley.cs.jqf.fuzz.util.IOUtils;
 public class ReproTfGuidance extends ReproGuidance {
 
     File[] inputFiles;
+    List<Integer> coverageSize;
 
     File tempDir;
     public ReproTfGuidance(File[] inputFiles, File traceDir) throws IOException {
         super(inputFiles, null);
         tempDir = traceDir;
         coverage = new ReproTfCoverage();
+        coverageSize = new ArrayList<Integer>();
     }
 
     public ReproTfGuidance(File inputFile, File traceDir) throws IOException {
@@ -40,39 +43,76 @@ public class ReproTfGuidance extends ReproGuidance {
     }
 
     @Override
+    public void handleResult(Result result, Throwable error) {
+        super.handleResult(result, error);
+        int sum = 0;
+
+        for(ReproTfCoverage.BranchData br: ((ReproTfCoverage) coverage).branchData) {
+            sum += br.trueTaken > 0 ? 1 : 0;
+            sum += br.falseTaken > 0 ? 1 : 0;
+        }
+
+        coverageSize.add(sum);
+    }
+
+    @Override
     public void save() {
         ReproTfCoverage cov = (ReproTfCoverage) coverage;
         List<ReproTfCoverage.CoverageData> coverageData = cov.getCoverageData();
-        String header = "class,method,iid,line,true,false";
+        String header = "class,method,iid,line,true,false,inference";
         int phase = 0 ;
 
         for(ReproTfCoverage.CoverageData cv: coverageData) {
             File f = new File(tempDir, String.format("/tf-log-%d.csv", phase));
             phase += 1;
-            IidBranchMap branchMap = cv.iidMapping;
-            ClassMethodMap classMethodMap = cv.classMethodMap;
-            MethodIidMap methodMapping = cv.methodMapping;
+            //IidBranchMap branchMap = cv.iidMapping;
+            //ClassMethodMap classMethodMap = cv.classMethodMap;
+            //MethodIidMap methodMapping = cv.methodMapping;
 
             List<String[]> data = new ArrayList<>();
-            for(Map.Entry<String, Set<String>> kv: classMethodMap.entrySet()) {
-                String[] row =new String[6];
+            for(Map.Entry<String, ReproTfCoverage.BranchData> kv: cv.branchData.entrySet()) {
+                String signature = kv.getKey();
+                ReproTfCoverage.BranchData br = kv.getValue();
+
+                String[] split = signature.split("\\.");
+                String cls = split[0];
+                String[] splitTwo = split[1].split(":");
+                String method = splitTwo[0];
+                String iid = splitTwo[1];
+
+                String[] row =new String[7];
                 data.add(row);
+                row[0] = cls;
+                row[1] = method;
+                row[2] = String.valueOf(iid);
+                row[3] = String.valueOf(iid);
+                row[4] = String.valueOf(br.trueTaken);
+                row[5] = String.valueOf(br.falseTaken);
+                row[6] = String.valueOf(cv.clazz + "." + cv.method + ":" + String.valueOf(cv.lineNumber));
+            }
+
+            /*
+            for(Map.Entry<String, Set<String>> kv: classMethodMap.entrySet()) {
                 String cls = kv.getKey();
                 Set<String> methods = kv.getValue();
-                row[0] = cls;
                 methods.forEach(method -> {
-                    row[1] = method;
                     Set<Integer> iids = methodMapping.get(method);
 
                     iids.forEach(branch -> {
+                        String[] row =new String[7];
+                        data.add(row);
+                        row[0] = cls;
+                        row[1] = method;
                         ReproTfCoverage.BranchData branchData = branchMap.get(branch);
                         row[2] = String.valueOf(branch);
                         row[3] = String.valueOf(branchData.lineNumber);
                         row[4] = String.valueOf(branchData.trueTaken);
                         row[5] = String.valueOf(branchData.falseTaken);
+                        row[6] = String.valueOf(cv.clazz + "." + cv.method + ":" + String.valueOf(cv.lineNumber));
                     });
                 });
             }
+            */
 
             PrintWriter pw = null;
             try {
@@ -85,9 +125,24 @@ public class ReproTfGuidance extends ReproGuidance {
 
             pw.write(header + "\n");
             for(String[] row: data) {
-                pw.format("%s,%s,%s,%s,%s,%s\n", row[0], row[1], row[2], row[3], row[4], row[5]);
+                pw.format("%s,%s,%s,%s,%s,%s,%s\n", row[0], row[1], row[2], row[3], row[4], row[5], row[6]);
             }
             pw.close();
         }
+
+        File f = new File(tempDir, "/coverage_size");
+        PrintWriter pw = null;
+        try {
+            f.createNewFile();
+            pw = new PrintWriter(f);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        for(Integer data: coverageSize) {
+            pw.println(data);
+        }
+
+        pw.close();
     }
 }
